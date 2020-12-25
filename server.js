8ff9a766eac5fe6dcb3fbdd85784e013a5e5e47d
Swayam Samyak Debasis <1234swayamsamyak@gmail.com>
2020-12-25 15:51:06 +0530

    make it better

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ connection.once("open", () => {
 const mainRouter = require("./routes/main");
 app.use("/main", mainRouter);
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
 });
